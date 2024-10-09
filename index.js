263dc2fc51a8091bd2278b37bd8d9d39b10e8ae4
giovane7171 <giovanenascimento46@gmail.com>
2024-10-09 15:01:01 -0400

    Update index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+    function togglesidebar(){
+        const sidebar = document.getElementById(`sidebar`);
+        
+if (sidebar.style.display === 'block') {
+
+  sidebar.style.display = 'none';
+}else {
+
+    sidebar.style.display = 'block';
+}
+
+    }
